eba36c0c5022ded2a7699a7dcc0600b4e170da6c
BuildTools <unconfigured@null.spigotmc.org>
2017-11-08 21:15:56 -0500

    stuff

--- app/models/room.js
+++ app/models/room.js
@@ -1,12 +1,16 @@
 const TinyQueue = require('tinyqueue');
 const Moniker = require('moniker');
 const redis = require("redis");
+const client = redis.createClient();
 
 class Room {
 
     constructor (apiKey) {
         name = Moniker.generator([Moniker.adjective, Moniker.noun]).choose();
         // TODO check in redis that name is unique
+        client.send_command('EXISTS ${name}', res => {
+
+        })
         this.apiKey = apiKey;
         this.songQueue = new TinyQueue([], a,b => {
             return a.votes > b.votes ? -1 : (a.votes < b.votes ? 1 : 0);
